645a06e297a3429b4f2c096877ab4f0ee2f46e0c
Star Mdakane <mmdakane@rocketmail.com>
2026-09-02 04:12:21 +0200

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Instrument_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const instrument = Instrument_Sans({
+  weights: [400, 600, 700],
   subsets: ["latin"],
-});
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={instrument.className}>{children}</body>
     </html>
   );
 }
---
+++ app/preview/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserPreviewPage = () => {
+    return (
+        <div>UserPreviewPage</div>
+    )
+}
+
+export default UserPreviewPage
